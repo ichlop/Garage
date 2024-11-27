4dfbdc2929b9e0f592bd7602bc1415f95626b744
Ioannis Chloptsios <86961394+ichlop@users.noreply.github.com>
2024-11-27 17:17:12 +0200

    Add files via upload

---
+++ SpringGarage/src/main/java/project/hire/springGarage/SpringGarageApplication.java
@@ -0,0 +1,13 @@
+package project.hire.springGarage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGarageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGarageApplication.class, args);
+	}
+
+}
---
+++ SpringGarage/src/test/java/project/hire/springGarage/SpringGarageApplicationTests.java
@@ -0,0 +1,13 @@
+package project.hire.springGarage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGarageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
